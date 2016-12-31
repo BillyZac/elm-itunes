dbf943e27d1da87ad44c106d78fa029f3214cca3
Zachary Smith <william.smith1@wipro.com>
2016-12-31 18:24:09 -0500

    Initial commit

---
+++ main.elm
@@ -0,0 +1,66 @@
+import Html exposing (text, ul, div, audio, p, h1)
+import Html.Attributes exposing (class, src, id, type_, controls)
+
+viewTune tune =
+  div [ class "audio-player" ]
+      [ p [] [ text tune.name ]
+      , audio
+          [ src tune.source
+          , id "audio-player"
+          , type_ "audio/ogg"
+          , controls True
+          ]
+          []
+      ]
+
+viewAlbum album =
+  div []
+  [ h1 [] [ text album.title]
+  , ul [] (List.map viewTune album.tracks)
+  ]
+
+main =
+  div []
+  [ viewAlbum album1
+  , viewAlbum album2
+  ]
+
+
+
+-- DATA
+
+album1 =
+  { title = "Grow Up"
+  , tracks = [
+    { name = "hey"
+    , source = "http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg"
+    , type_ = "audio/ogg"
+    }
+  , { name = "you"
+    , source = "http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg"
+    , type_ = "audio/ogg"
+    }
+  , { name = "guys"
+    , source = "http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg"
+    , type_ = "audio/ogg"
+    }
+  ]
+  }
+
+album2 =
+  { title = "Die Young"
+  , tracks = [
+    { name = "hey"
+    , source = "http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg"
+    , type_ = "audio/ogg"
+    }
+  , { name = "you"
+    , source = "http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg"
+    , type_ = "audio/ogg"
+    }
+  , { name = "guys"
+    , source = "http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg"
+    , type_ = "audio/ogg"
+    }
+  ]
+  }
